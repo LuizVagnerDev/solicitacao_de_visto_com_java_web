4babe97a73f09ecf41b8ca763da8636c9f29155b
Luiz Vagner da Silva <luizvagnerdev@gmail.com>
2024-11-10 15:21:06 -0300

    removidos os arquivos anteriores para realizar a conexao com o bd

--- src/main/java/resources/static/js/lista.js
+++
@@ -1,5 +0,0 @@
-let lista_de_cadastros = document.querySelector('.ver_lista');
-lista_de_cadastros.addEventListener('click', event =>{
-    console.log('click')
-    window.open("./lista_de_cadastros.html", "_blank");
-})
